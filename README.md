102597d4faf429d7100dfdea9c766d7427263e94
Scott Wadden <scott.wadden@emc.com>
2014-12-11 06:49:25 -0400

    Auto install packages from .brackets-extensions/autoinstall.json

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 project-extensions
 ==================
 
-Load brackets extensions automatically from the .brackets-extensions directory in a project.
+Load brackets extensions automatically from the .brackets-extensions directory in a project. Automatically install any package
+listed in .brackets-extensions/autoinstall.json from the registry.
--- main.js
+++ main.js
@@ -5,6 +5,11 @@ define(function (require, exports, module) {
     var FileSystem  = brackets.getModule("filesystem/FileSystem"),
         ProjectManager = brackets.getModule("project/ProjectManager"),
         ExtensionLoader = brackets.getModule("utils/ExtensionLoader"),
+        FileUtils = brackets.getModule("file/FileUtils"),
+        Package = brackets.getModule("extensibility/Package"),
+        ExtensionManager = brackets.getModule("extensibility/ExtensionManager"),
+        Dialogs = brackets.getModule("widgets/Dialogs"),
+        DefaultDialogs = brackets.getModule("widgets/DefaultDialogs"),
         _oldOpenProject = ProjectManager.openProject;
     
     ProjectManager.openProject = function(path) {
@@ -13,10 +18,45 @@ define(function (require, exports, module) {
 
         FileSystem.getDirectoryForPath(extensionDir).getContents(function (err) {
             if (!err) {
+                var autoInstallFile = FileSystem.getFileForPath(extensionDir + "/autoinstall.json");
+                FileUtils.readAsText(autoInstallFile).done(function(installJson) {
+                    var extensions = ExtensionManager.extensions;
+                    var install = JSON.parse(installJson).filter(function(name) {
+                        return !extensions[name] || !extensions[name].installInfo;
+                    });
+
+                    if (install.length) {
+                        Package._getNodeConnectionDeferred().then(function() {
+                            var promises = [];
+                            ExtensionManager.downloadRegistry().done(function(registry) {
+                                install.forEach(function(name) {
+                                    var extension = extensions[name];
+                                    if (!extension) {
+                                        Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_ERROR,
+                                            "Plugins AutoInstall Error", "Could not get package info for autoinstall extension: " + name);
+                                    }
+                                    else {
+                                        var latest = ExtensionManager.getCompatibilityInfo(extension.registryInfo, brackets.metadata.apiVersion);
+                                        var url = ExtensionManager.getExtensionURL(name, latest.compatibleVersion);
+                                        promises.push(Package.installFromURL(url).promise);
+                                    }
+                                    if (promises.length) {
+                                        $.when.apply($, promises).done(function() {
+                                            Dialogs.showModalDialog(DefaultDialogs.DIALOG_ID_INFO,
+                                                "Plugins AutoInstalled", "New plugins have been automatically installed. You should restart");
+                                        });
+                                    }
+                                });
+                            });
+                        });
+                    }
+                });
+
                 ExtensionLoader.loadAllExtensionsInNativeDirectory(extensionDir).then(function() {
                     d.resolve();
                 });
-            } else {
+            }
+            else {
                d.resolve();
             }
         });
